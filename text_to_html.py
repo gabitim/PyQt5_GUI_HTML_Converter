18730feae998ed8c95d42c285f5fbb6c84b62b51
Gabriel Timofti <gabitim470@gmail.com>
2020-04-04 19:05:34 +0300

    created the python script which converts text to html with rst2html from docutils

---
+++ text_to_html.py
@@ -0,0 +1,34 @@
+# helper script to convert text to html, using rst2html from docutils !! 
+# will be conected with the main script...
+
+# https://docs.python.org/3/library/pathlib.html
+from pathlib import Path
+
+# https://pypi.org/project/docutils/
+import docutils.io, docutils.core
+
+def rst2html(source_path):
+    # mostly taken from
+    # https://github.com/getpelican/pelican/
+    pub = docutils.core.Publisher(
+        source_class=docutils.io.FileInput,
+        destination_class=docutils.io.StringOutput)
+    pub.set_components('standalone', 'restructuredtext', 'html')
+    pub.process_programmatic_settings(None, None, None)
+    pub.set_source(source_path=str(source_path))
+    pub.publish()
+
+    html = pub.writer.parts['whole']
+
+    return html
+
+SRC_DIR = Path('.')
+DST_DIR = Path('.')
+
+# file_name is file we need to convert from  browse !!
+file_name = 'plain_text.txt'
+
+html = rst2html(file_name)
+
+with open(DST_DIR / (file_name[:-4] + '.html'), 'w') as f:
+	f.write(html)
